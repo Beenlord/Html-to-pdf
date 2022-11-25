051be8001e87a47a6d94f7bd567800a292431ffa
Vovikkko <45092587+Beenlord@users.noreply.github.com>
2022-11-25 16:19:20 +0300

    WIP

--- program.js
+++ program.js
@@ -1,10 +1,12 @@
 const chalk = require('chalk');
+const inquirer = require('inquirer');
 const express = require('express');
 const handlebars = require('handlebars');
 const puppeteer = require('puppeteer');
-const printer = require('node-printer');
 const path = require('path');
 const fs = require('fs');
+
+let properties = {};
 
 const app = express();
 app.use('/static',express.static(__dirname + '/static'));
@@ -19,6 +21,7 @@ app.get('/', async (req, res) => {
     
         res.send(template({
             title: 'Штрихкод',
+            ...properties,
         }));
     }
     catch {
@@ -26,32 +29,84 @@ app.get('/', async (req, res) => {
     }
 });
 
-const server = app.listen(8080, async () => {
-    console.log(chalk.green('Сервер запузщен на хосте localhost:8080'));
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'paper',
+            message: 'В каком формате печатать?',
+            choices: [
+                'A4',
+                'A5',
+                'A6',
+            ],
+        },
+        {
+            type: 'number',
+            name: 'fields',
+            message: 'Поля от краёв листа - ',
+            default: 0,
+        },
+        {
+            type: 'number',
+            name: 'width',
+            message: 'Максимальная ширина картинки в пикселях -',
+            default: 512,
+        },
+        {
+            type: 'number',
+            name: 'height',
+            message: 'Максимальная высота картинки в пикселях - ',
+            default: 512,
+        },
+    ]).then((opt) => {
+        const file = process.argv[2] && fs.existsSync(process.argv[2].split(path.sep).pop()) ? process.argv[2] : false;
 
-    const browser = await puppeteer.launch({
-        args: [
-            '--no-sandbox',
-        ],
-        headless: true,
-    });
+        if (file) {
+            
+            properties = {
+                ...properties,
+                ...opt,
+                fileName: path.join('static', file.split(path.sep).pop()),
+            };
+
+            console.log('Паратетры запуска:', properties);
     
-    const page = await browser.newPage();
-
-    await page.goto(`http://localhost:8080`, {
-        waitUntil: "networkidle0",
+            const server = app.listen(8080, async () => {
+                console.log(chalk.green('Сервер запузщен на хосте localhost:8080'));
+            
+                const browser = await puppeteer.launch({
+                    args: [
+                        '--no-sandbox',
+                    ],
+                    headless: true,
+                    executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+                });
+                
+                const page = await browser.newPage();
+            
+                await page.goto(`http://localhost:8080`, {
+                    waitUntil: "networkidle0",
+                });
+            
+                console.log(chalk.yellow('Обработка шаблона...'));
+            
+                await page.pdf({
+                    format: properties.paper,
+                    // margin: {
+                    //     top: "40px",
+                    //     bottom: "100px",
+                    // },
+                    // printBackground: true,
+                    path: "default.pdf",
+                });
+            
+                // console.log(chalk.green('Готово.'));
+                
+                await browser.close();
+                // await server.close();
+            });
+        } else {
+            console.log(chalk.red('X'), 'Путь к файлу изображения указан не верно или более не актуален.');
+        }
     });
-
-    await page.pdf({
-        format: "A4",
-        // margin: {
-        //     top: "40px",
-        //     bottom: "100px",
-        // },
-        // printBackground: true,
-        path: "default.pdf",
-    });
-    
-    await browser.close();
-    await server.close();
-});
